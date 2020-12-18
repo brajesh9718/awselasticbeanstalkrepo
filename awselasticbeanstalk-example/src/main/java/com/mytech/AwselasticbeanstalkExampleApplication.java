17723c2dbbbf59d93542cfb0067d61a5d4fd74fa
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2020-12-18 17:05:27 +0530

    first commit

---
+++ awselasticbeanstalk-example/src/main/java/com/mytech/AwselasticbeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwselasticbeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwselasticbeanstalkExampleApplication.class, args);
+	}
+
+}
