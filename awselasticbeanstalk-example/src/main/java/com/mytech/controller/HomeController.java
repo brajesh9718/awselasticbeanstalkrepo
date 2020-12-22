d764b78fc1c86629f7902abb67ef76bb94758474
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2020-12-22 16:38:15 +0530

    Add new Service......

--- awselasticbeanstalk-example/src/main/java/com/mytech/controller/HomeController.java
+++ awselasticbeanstalk-example/src/main/java/com/mytech/controller/HomeController.java
@@ -1,13 +1,84 @@
 package com.mytech.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mytech.cicdpipelineapp.dto.Address;
+import com.mytech.cicdpipelineapp.dto.User;
+import com.mytech.cicdpipelineapp.service.Calculator;
+import com.mytech.cicdpipelineapp.service.CalculatorFuntionalInterface;
 
 @RestController
 public class HomeController {
+	
+	ArrayList<User> users = new ArrayList<User>();
 
 	@GetMapping("/")
 	public String sayHello() {
 		return "Let me Change this .......!!!!!!!";
+	}
+
+	// URL to request =
+	// http://localhost:<Port>/spring-mvc-example/getUserById/102?no=3
+	@GetMapping(value = "/getUserById/{id}")
+	public User getUserById(@PathVariable int id) {
+		try {
+			Address address = new Address();
+			address.setAid(2);
+			address.setAdd("Mathura");
+			User user1 = new User();
+			user1.setUid(101);
+			user1.setUsername("Ram");
+			user1.setAddress(address);
+			User user2 = new User();
+			user2.setUid(102);
+			user2.setUsername("Shyam");
+			user2.setAddress(address);
+			User user3 = new User();
+			user3.setUid(103);
+			user3.setUsername("Radha");
+			user3.setAddress(address);
+			users.add(user1);
+			users.add(user3);
+			users.add(user2);
+
+			// Using Jdk1.8 Stream API
+			User userObj = users.stream().filter(data -> data.getUid() == id).findFirst().get();
+			return userObj;
+		} catch (Exception e) {
+			System.out.println("Exception occur " + e);
+		}
+		return null;
+	}
+
+	// Use Lamda Expression by rest call
+	@GetMapping(value = "/add")
+	public String getCalc(@RequestParam int no1, @RequestParam int no2) {
+		CalculatorFuntionalInterface calc = (a, b) -> {
+			return a + b;
+		};
+		return "Addition of numbers :: " + calc.add(no1, no2);
+	}
+
+	@GetMapping(value = "/getFactorial/{no}")
+	public int getFactorial(@PathVariable int no) {
+		Calculator cal = (x) -> {
+			int fact = 1;
+			if (x == 0)
+				return 1;
+			else if (x > 0) {
+				for (int i = 1; i <= x; i++) {
+					fact = fact * i;
+				}
+				return fact;
+			}
+			return fact;
+
+		};
+		return cal.getFactorial(no);
 	}
 }
